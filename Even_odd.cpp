d8b577a15a9c4c9d3abd80cd499817fc435fc35b
sahilbhiwankar34 <sahilbhiwankar34@gmail.com>
2026-01-05 19:58:22 +0530

    Add files via upload

---
+++ Even_odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a;
+	cout<<"Enter the numbers: ";
+	cin>>a;
+	if(a%2==0){
+		cout<<"even number";
+	}
+	else{
+		cout<<"not even";
+	}
+	return 0;
+}
