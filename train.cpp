17800d79326048a394f315747f46fceb6faa2b6c
sahilbhiwankar34 <sahilbhiwankar34@gmail.com>
2026-01-05 19:54:56 +0530

    Add files via upload

---
+++ train.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char train[30];
+	cin>>train;
+	if(train==Vande){
+		float bill=2000*9.1;
+		cout<<bill;
+	}
+	else if(tain==Mumbai){
+		float bills=3000*9.1;
+		cout<<bill;
+	}
+	else{
+		float bil=1000*9.1;
+		cout<<bill;
+	}
+	return 0;
+}
