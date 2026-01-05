07c618d7df1b9ce015f965a9df9a2f1892105dd3
sahilbhiwankar34 <sahilbhiwankar34@gmail.com>
2026-01-05 19:55:14 +0530

    Add files via upload

---
+++ switch_days.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num;
+	cout<<"Enter the number from 1 to 7: ";
+	cin>>num;
+	switch(num){
+		case 1:
+			cout<<"Monday";
+			break;
+			case 2:
+			cout<<"Tuesday";
+			break;
+			case 3:
+			cout<<"Wednesday";
+			break;
+			case 4:
+			cout<<"Thursday";
+			break;
+			case 5:
+			cout<<"Friday";
+			break;
+			case 6:
+			cout<<"Saturday";
+			break;
+			case 7:
+			cout<<"Sunday";
+			break;
+			default:
+				cout<<"Invalid entry please enter number from 1 to 7";
+	}
+	return 0;
+}
