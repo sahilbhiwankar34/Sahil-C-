7bd6b7d3c98d235807142f8e219937246715d2c9
sahilbhiwankar34 <sahilbhiwankar34@gmail.com>
2026-01-05 19:55:46 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+ main(){
+	int a,b;
+	int sub,add,mul,div,mod;
+	cout<<"The value of a and b is: ";
+	cin>>a>>b;
+	add=a+b;
+	sub=a-b;
+	mul=a*b;
+	div=a/b;
+	mod=a%b;
+		cout<<"add of a and b is:"<<add;
+cout<<"\nsub of a and b is:"<<sub;
+
+cout<<"\nmul of a and b is:"<<mul;
+
+cout<<"\ndiv of a and b is:"<<div;
+
+cout<<"\nmod of a and b is:"<<mod;
+
+}
